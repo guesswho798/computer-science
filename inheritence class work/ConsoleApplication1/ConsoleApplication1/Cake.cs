fea4b4a2a1a41716d8a5f46a88b9edba831fb253
guesswho798 <36185834+guesswho798@users.noreply.github.com>
2020-05-13 13:13:30 +0300

    inheritance class work
    just some class work i did in school
    was fun
    got to refresh my memory

---
+++ inheritence class work/ConsoleApplication1/ConsoleApplication1/Cake.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Cake : Dessert
+    {
+        private int numpieces;
+        public Cake(string name, int cost, int amount, int numpieces) : base(name, cost, amount)
+        {
+            this.Name = name;
+            this.Cost = cost;
+            this.Amount = amount;
+            this.numpieces = numpieces;
+        }
+
+        public int piecePrice()
+        {
+            return this.Cost / this.numpieces;
+        }
+    }
+}
